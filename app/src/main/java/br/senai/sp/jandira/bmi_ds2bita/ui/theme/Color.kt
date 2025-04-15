cba34a1f26b73e570aa732126226ff0c613e06ba
BRUNO DIAS GONÇALVES <24122451@dsn127m1249388.cfp127aluno.local>
2025-04-15 16:57:25 -0300

    finalizado

---
+++ app/src/main/java/br/senai/sp/jandira/bmi_ds2bita/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package br.senai.sp.jandira.bmi_ds2bita.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Blue80 = Color(0xFF80B3FF)  // Azul claro
+val BlueGrey80 = Color(0xFFA8B9D8)  // Azul acinzentado claro
+val SkyBlue80 = Color(0xFF87CEEB)  // Azul céu
+
+val Blue40 = Color(0xFF2F6B9D)  // Azul médio
+val BlueGrey40 = Color(0xFF4A6475)  // Azul acinzentado médio
+val NavyBlue40 = Color(0xFF3B4C6D)  // Azul marinho
